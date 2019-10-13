9c58b7ce8f1fe5dbd727337217feb062a30d6517
Francesco <francesco.argentieri89@gmail.com>
2019-10-13 14:12:30 +0200

    fix: tests

--- dronelanding/traje/__init__.py
+++
@@ -1,6 +0,0 @@
-from raspberrycamera import RaspberryPiCamera
-from trajectory import PI
-from trajectory import rad2degree
-from trajectory import degree2rad
-from trajectory import Trajectory
-from trajectory import Transformation
